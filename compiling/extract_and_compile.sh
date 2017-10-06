accc1a5cbb4c13f0f406a4710331d8c5d9285bd7
Kyle M Foss <fossx229@multivac.morris.umn.edu>
2017-10-06 14:25:57 -0500

    --test

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/bash
+#Saving the prime number we are given
 PrimeNumber=$1
+#Saves the directory given as SCRATCH
 SCRATCH=$2
+#unzips given compressed fles
 tar -zxf NthPrime.tgz --directory=$SCRATCH
+#Changes into the unzipped directory
 cd $SCRATCH/NthPrime
+#compiles NthPrime
 gcc -o NthPrime *.c
+#runs NthPrime
 ./NthPrime $PrimeNumber
 
 
